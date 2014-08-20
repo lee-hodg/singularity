f82ed7af432160a8a1a8a02ff7730f6f4f130904
fpghost <leehodg@gmail.com>
2014-08-20 17:17:49 +0200

    Comment level filter and way to check if comment has children. data-level added to uls to reflect the comment level. Show replies button added to parents with children which with some js, can show initially hidden children of parent

--- theme/templatetags/templateutils.py
+++ theme/templatetags/templateutils.py
@@ -46,3 +46,40 @@ def splitcolor(word, args):
         pass
 
     return word
+
+
+@register.filter(name='defaultdict_keys')
+def defaultdict_keys(ddict):
+    '''
+    Normally in the template given a dictionary
+    called mydict in the context, keys() can be called
+    simply with {{mydict.keys}}.  However because of the
+    way Django does look ups, namely first trying to
+    see if a key called 'keys' exists and only trying keys()
+    method if not, and because of the nature of the defaultdict(list)
+    , namely returning [] even if the key lookup failed and creating
+    a key called 'keys', {{mydict.keys}} will just result in []
+    not the actual keys of the defaultdict.
+
+    This filter grabs the existings keys of a defaultdict.
+    '''
+    return ddict.keys()
+
+@register.filter(name='comment_level')
+def comment_level(comment):
+    '''
+    Takes a comment and returns its level in the tree heirarchy.
+    Parent comments are not replies to anything and have level 0.
+    '''
+    level = 0
+    while True:
+        if not comment.replied_to:
+            # comment is not a reply, but a level 0 parent.
+            break
+        else:
+            # comment is a reply, inc level +1
+            level += 1
+            # move to comment above
+            comment = comment.replied_to
+        # at end of recursion rtn level
+    return level
