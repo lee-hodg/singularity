3b3cf421fe99a58d39c6b0394cd719f18366ba88
Lee H <leehodg@gmail.com>
2014-08-22 01:12:05 +0100

    Attempt toward comment JSON/AJAX view

--- theme/views.py
+++ theme/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from mezzanine.generic.templatetags.comment_tags import comments_for
+from django.template.loader import render_to_string
 
-# Create your views here.
+
+def ajax_comments(request, pk):
+    '''
+    Render to string the comments for a given BlogPost or
+    whatever.
+    '''
+    context["request"] = request
+    obj = BlogPost.objects.filter(pk=pk)
+    context = comments_for(context, obj)
+    comments_str = render_to_string("generic/includes/comments.html",
+                                    RequestContext(request, context))
+    response_data = {}
+    response_data['comments'] = comments_str
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
